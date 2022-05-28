f99d082fb3ecfb8636869df86ffa5a19169ecdc8
Raven <purplebox02@gmail.com>
2022-05-28 10:34:51 +0300

    JS Completed

--- main.js
+++ main.js
@@ -1,51 +1,100 @@
 const wordElement = document.getElementById('word');
+const wrongGuesses = document.getElementById('wrong-letter-display');
+const inputCheck = document.getElementById('input-check');
+const notification =document.getElementById('notification');
+const notifText = document.getElementById('notif-text');
+const parts = document.querySelectorAll('.sad-dude-part');
+const body = document.querySelector('main');
+const restart = document.getElementById('restart-btn');
 
-const words = ["java", "python", "patates", "sucuk", "raven", "jerjer"]
+const words = ["java", "python", "patates", "sucuk", "raven", "jerjer"];
 
 selectedWord = words[Math.floor(Math.random() * words.length)];
 console.log(selectedWord);
 
 let correctLetters = [];
 let wrongLetters = [];
-let selectedWordArray = selectedWord.split('');
 
-window.addEventListener('keydown', e => {
+window.addEventListener('keydown', keyDownEvent);
+
+function keyDownEvent(e) {
     let guessedLetter = e.key;
     console.log(guessedLetter);
-    
+    if(e.keyCode >= 65 && e.keyCode <= 90 || e.keyCode ==222) {
         if(selectedWord.includes(guessedLetter)) {
             if(!correctLetters.includes(guessedLetter)) {
                 correctLetters.push(guessedLetter);
                 loadWords();
-                console.log(correctLetters);
             } else {
-                return
+                let e = "You Have Already Entered This Letter!";
+                showInputCheck(e);
             }; 
         } else {
             if(!wrongLetters.includes(guessedLetter)) {
                 wrongLetters.push(guessedLetter);
-                console.log(wrongLetters);
-            };
+                loadWrongLetters();
+                loadParts();
+                loseCheck();
+            } else {
+                let e = "You Have Already Entered This Letter!";
+                showInputCheck(e);
+            }
         };
-
-});
+    } else {
+        let e = "Please Enter A Valid Letter!";
+        showInputCheck(e);
+    }
+};
 
 function loadWords() {
-    selectedWordArray.forEach(letter => {
-        let letterLines = document.createElement('div');
-        letterLines.className = 'line';
-        if(correctLetters.includes(letter)) {
-            letterLines.textContent = `${letter}`;
-        } else {
-            letterLines.textContent = ' ';
-        }
+    wordElement.innerHTML = `${selectedWord.split('').map(letter => `<p class="line">${correctLetters.includes(letter) ? letter : ""}</p>`).join('')}`;
+    const fullWord = wordElement.innerText.replace(/\n/g, "");
 
-        wordElement.appendChild(letterLines);
+    if(fullWord === selectedWord.toLocaleUpperCase("en")) {
+        notification.style.display = "flex";
+        notification.style.backgroundColor = "rgb(26, 226, 59)";
+        notifText.textContent = "Congratulations!"
+        body.style.opacity = "0.5";
+        window.removeEventListener('keydown', keyDownEvent);
+    };
+};
+
+function showInputCheck(e) {
+    inputCheck.innerHTML = `${e}`;
+    inputCheck.style.display = "block";
+    setTimeout(() => {
+        inputCheck.style.display = "none";
+    }, 1500);
+};
+
+function loadWrongLetters() {
+    wrongGuesses.textContent = `${wrongLetters}`;
+};
+
+function loseCheck() {
+    if(wrongLetters.length == 6) {
+        notification.style.display = "flex";
+        notification.style.backgroundColor = "rgb(172, 38, 38)";
+        notifText.textContent = "You Lost!"
+        notifText.style.color = "white";
+        body.style.opacity = "0.5";
+        window.removeEventListener('keydown', keyDownEvent);
+    };
+};
+
+function loadParts() {
+    parts.forEach((part, index) => {
+        if(index < wrongLetters.length) {
+            part.style.display = "block";
+        } else {
+            part.style.display = 'none';
+        };
+        console.log(part);
     });
 };
 
-// function alreadyEntered() {
-
-// };
+restart.addEventListener('click', e => {
+    document.location.reload(true);
+});
 
 loadWords();
